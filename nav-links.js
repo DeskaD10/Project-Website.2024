786f6613a6d8eb8612abab10cfe437b48ce6d840
DeskaD10 <128162312+DeskaD10@users.noreply.github.com>
2024-02-04 11:02:28 +0200

    Update nav-links.js

--- nav-links.js
+++ nav-links.js
@@ -1,18 +1,14 @@
-const navLinkElements = document.querySelectorAll(".nav_link");
-const windowPathname = window.location.pathname;
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinkElements = document.querySelectorAll(".nav_link");
+  const windowPathname = window.location.pathname;
 
-navLinkElements.forEach((navLinkEl) => {
-  const navLinkPathname = new URL(navLinkEl.href).pathname;
-  if (
-    windowPathname === navLinkPathname ||
-    (windowPathname === "./index.html" && navLinkPathname === "/")
-  ) {
-    navLinkEl.classList.add("active");
-  }
-
-  //   navLinkEl.addEventListener("click", (event) => {
-  //     event.preventDefault();
-  //     const targetPathname = new URL(event.currentTarget.href).pathname;
-  //     window.location.href = targetPathname;
-  //   });
+  navLinkElements.forEach((navLinkEl) => {
+    const navLinkPathname = new URL(navLinkEl.href).pathname;
+    if (
+      windowPathname === navLinkPathname ||
+      (windowPathname === "/" && navLinkPathname === "/index.html")
+    ) {
+      navLinkEl.classList.add("active");
+    }
+  });
 });
